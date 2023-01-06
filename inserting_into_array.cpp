1761e0aef1ba780daffdb92c61ce09e8c3245d0d
BhavinOndhiya <62885659+BhavinOndhiya@users.noreply.github.com>
2023-01-06 22:55:22 +0530

    Update inserting_into_array.cpp

--- inserting_into_array.cpp
+++ inserting_into_array.cpp
@@ -1,17 +1,17 @@
 //Displaying the Elements
 for( i= 0; i < length; i++ )
-	 	 {
-	 	 	 print( A[ i ] )
-	 	 }
+{
+	 print( A[ i ] )
+}
  
 //Inserting the Elements
  Insert(4, 15)
-	 	 	 for( i = Length; i > index ; i - -)
-	 	 	 {
-	 	 	 	 A[ i ] = A[ i -1] ;
-	 	 	 }
-	 	 	 A[ index ] = x ;
-	 	 	 Length ++;
+ for( i = Length; i > index ; i - -)
+{
+	 A[ i ] = A[ i -1] ;
+}
+ A[ index ] = x ;
+ Length ++;
        
 #include<stdio.h>
 struct Array
@@ -46,7 +46,7 @@ struct Array
 int main()
 {
 struct Array arr1={{2,3,4,5,6},10,5};
-Append(&arr1,10);
+Append(&arr1,10);//CALLING THE FUNCTIONS WE HAVE CREATED
 Insert(&arr1,0,12);
 Display(arr1);
  return 0;
