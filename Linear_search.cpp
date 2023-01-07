f02f9da323dc3022e86d91c52230560cbc3e7cdc
BhavinOndhiya <62885659+BhavinOndhiya@users.noreply.github.com>
2023-01-07 18:28:19 +0530

    Add files via upload

---
+++ Linear_search.cpp
@@ -0,0 +1,8 @@
+for( i = 0; i < length ; i++ )
+	 	 {
+	 	 	 if( key == A[ i ] )
+	 	 	 return i;	 	 	 //if search is successful it ends here
+	 	 }
+	 	 
+
+	 	 return -1;	 	 	 	 // if search unsuccessful returns -1
